421a5babd11e8122a185309ffbd27453660e2fc1
Aditya <adityasinghbtp123@gmail.com>
2025-01-17 12:18:09 +0530

    first commit

---
+++ Division.java
@@ -0,0 +1,18 @@
+package CapgeminiTrainingDay2;
+
+import java.util.Scanner;
+
+public class Division {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try (Scanner s = new Scanner(System.in)) {
+			int a=s.nextInt();
+			int b=s.nextInt();
+			int c=s.nextInt();
+			
+			System.out.println((a/b)/c);
+		}
+	}
+
+}
